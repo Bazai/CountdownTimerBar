7a01dee54f73d75af1b89ac94228a8718664cabd
Pavel Bubentsov <bubentsov@evrone.team>
2025-06-27 19:12:43 +0300

    Initial Commit

---
+++ CountdownTimerBar/CountdownTimerBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownTimerBarApp.swift
+//  CountdownTimerBar
+//
+//  Created by Pavel Bubentsov on 27.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct CountdownTimerBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
